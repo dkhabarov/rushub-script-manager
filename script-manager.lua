d717f8a554e4e59489899ab4b1d5c019db0faae1
Saymon21 <saymon@hub21.ru>
2013-03-20 01:15:34 +0400

    new file:   lang/scripts/English/script-manager.lang new file:   lang/scripts/Russian/script-manager.lang modified:   script-manager.lua ------------------------------ Version 0.2: Refactoring; Standardization for the interoperability with other my scripts; Adding multi-language support;

--- script-manager.lua
+++ script-manager.lua
@@ -14,28 +14,41 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -- ***************************************************************************
-_MYVERSION = "0.1"
+-- Bug reports and feature requests please send to issue tracker at:
+-- http://opensource.hub21.ru/rushub-script-manager/issues
+-- ***************************************************************************
 -- SETTINGS:
-def_allow_prefix= {["!"]=true, ["+"]=true} -- Definition allowed prefixes. 
-def_acl = { -- Definition profiles and acl for commands, etc
-	[0]= {
-		tcmds={
-			["startscript"] = true,
-			["stopscript"] = true,
-			["lsscript"] = true,
-			["movedownscript"] = true,
-			["moveupscript"] = true,
-			["restartscript"] = true,
+def_config = {
+	allow_prefix= {["!"]=true, ["+"]=true}, -- Definition allowed prefixes. 
+	acl = { -- Definition profiles and acl for commands etc
+		[0]= {
+			tcmds={
+				["startscript"] = true,
+				["stopscript"] = true,
+				["lsscript"] = true,
+				["movedownscript"] = true,
+				["moveupscript"] = true,
+				["restartscript"] = true,
 			
+			}
 		}
 	}
 }
 -- ***************************************************************************
+_MYVERSION = "0.2"
+_TRACEBACK = debug.traceback	
+lang_file = Config.sLangPath.."scripts/"..Config.sLang.."/script-manager.lang" -- Definition language file
+dofile(lang_file) -- Load language file
+
+function OnStartup()
+	botname = Config.sHubBot
+end
+
 def_commands = { -- commands
-	["startscript"] = {
-		
-		-- short_description="Start script",
-		-- user_command = {{1, 3, "Move start script", "!%s %[line:Enter script name]"}}
+	["startscript"] = { -- definition command name startscript 
+		options={
+			short_description = lang[1],
+		},
 		["acl"] = function (UID,cmd, sData) -- check access
 			return check_acl(UID, cmd)
 		end,
@@ -45,20 +58,20 @@ def_commands = { -- commands
 		["cmd"] = function(UID, cmd, sData)
 			if cmd then
 				if not sData or sData == "" then
-					return true, "Invalid arguments. See !"..cmd.." -h for more details."
+					return true, lang[2]:format(cmd)
 				end
 				local ascript = Core.GetScript(sData) or {}
 				if not ascript.sName then
-					return true, "Script \""..sData.."\" not found!"
+					return true, lang[3]:format(sData)
 				elseif ascript.bEnabled then
-					return true, "Script \""..sData.."\" is already running!"
+					return true, lang[4]:format(sData)
 				else
 					local err = false
 					local _, err = Core.StartScript(ascript.sName)
 					if err then
-						return true, "Error when starting script \"".. ascript.sName.. " \": "..err
+						return true, lang[5]:format(ascript.sName, err)
 					else
-						return true, "\""..ascript.sName.."\": successfully loaded & initialized"
+						return true, lang[6]:format(ascript.sName)
 					end
 				end				
 			end
@@ -66,15 +79,15 @@ def_commands = { -- commands
 
 		-- Documentation for this command.
 		["man"] = function(UID, cmd, sData)
-			return true, "\r\nNAME:\r\n\t!"..cmd..
-				"\r\nSYNOPSIS:\r\n\t"..cmd..
-				" [ -h ] [ scriptname ]\r\nDESCRIPTION:\r\n\t"..cmd..
-				" - command to load scripts.\r\nOPTIONS:\r\n\t-h\t Show this help\nEXAMPLE USAGE:\r\n\t!".. cmd.." ".. Core.GetScript().sName.."\r\nSEE ALSO:\r\n\t !stopscript, !lsscript, !movedownscript, !moveupscript, !restartscript"
-		end,
-		
+			return true, lang[7]:gsub("%[(%S+)%]", {CMD = cmd, CURNAME=Core.GetScript().sName})
+		end,	
 	}, -- command startscript end 
 	
 	["stopscript"] = {
+		options={
+			short_description=lang[8],
+		},
+		
 		["acl"] = function(UID, cmd, sData)
 			return check_acl(UID,cmd)
 		end,
@@ -82,67 +95,68 @@ def_commands = { -- commands
 		["cmd"] = function (UID, cmd, sData)
 			if cmd then
 				if not sData or sData == "" then
-					return true, "Invalid arguments. See !"..cmd.." -h for more detals."
+					return true, lang[2]:format(cmd)
 				end
 				local ascript = Core.GetScript(sData) or {}
 				if not ascript.sName then
-					return true, "Script \""..sData.."\" not found!"
+					return true, lang[3]:format(sData)
 				elseif not ascript.bEnabled then
-					return true, "Script \""..sData .."\" is already stopped!"
+					return true, lang[9]:format(sData)
 				else
 					local err = false
 					local _, err = Core.StopScript(ascript.sName)
 					if err then
-						return true, "Error when stopping script \"".. ascript.sName.. " \": "..err
+						return true, lang[10]:format(ascript.sName,err)
 					else
-						return true, "Script \""..ascript.sName.."\" successfully unloaded"
+						return true, lang[11]:format(ascript.sName)
 					end
 				end	
 			end
 		end,
 	
 		["man"] = function (UID, cmd, sData)
-			return true, "\r\nNAME:\r\n\t!"..cmd..
-				"\r\nSYNOPSIS:\r\n\t"..cmd..
-				" [ -h ] [ scriptname ]\r\nDESCRIPTION:\r\n\t"..cmd..
-				" - command to unload scripts.\r\nOPTIONS:\r\n\t-h\t Show this help\nEXAMPLE USAGE:\r\n\t!".. cmd.." ".. Core.GetScript().sName.."\r\nSEE ALSO:\r\n\t !startscript, !lsscript, !movedownscript, !moveupscript, !restartscript"
+			return true, lang[12]:gsub("%[(%S+)%]", {CMD = cmd, CURNAME=Core.GetScript().sName})
 		end,
 	},
 	
 	["lsscript"] = {
+		options={
+			short_description=lang[15],
+		},
+	
 		["acl"] = function (UID, cmd, sData)
 			return check_acl(UID, cmd)
 		end,
 		
 		["cmd"] = function (UID, cmd, sData)
 			if cmd then
-				local msg_header="\r\n\tID [ STATE ] \t         NAME              MEMUSAGE\r\n"
 				if not sData then
 					local script_list=""
 					for i, v in ipairs(Core.GetScripts()) do
-						script_list = script_list..(("\t%s [ %s ] \t%s%s\n"):format(i > 9 and i or "0"..i, v.bEnabled and "enabled" or "disabled", v.sName, v.iMemUsage ~= 0 and (" (%s kb)"):format(v.iMemUsage) or ""))
+						script_list = script_list..(("\t%s\t%s\t\t%s\t\t%s\n"):format(i > 9 and i or "0"..i, v.bEnabled and lang[13] or lang[14], v.sName, v.iMemUsage ~= 0 and (" (%s kb)"):format(v.iMemUsage) or ""))
 					end
-					return true, msg_header..script_list	
+					return true, lang[16]..script_list	
 				elseif sData ~= nil then
 					script=Core.GetScript(sData)
 					if not script.sName then
-						return true, "Script \""..sData.."\" not found!"
+						return true, lang[3]:format(sData)
 					else
-						return true, (("\nInformation for script \"%s\":\r\n\tState: %s\r\n\tMemUsage: %s\r\n"):format(script.sName, script.bEnabled and "enabled" or "disabled", script.iMemUsage ~= 0 and (" (%s kb)"):format(script.iMemUsage) or ""))	
+						return true, lang[17]:format(script.sName, script.bEnabled and lang[13] or lang[14], script.iMemUsage ~= 0 and (" (%s kb)"):format(script.iMemUsage) or "")	
 					end
 				end
 			end
 		end,
 	
 		["man"] = function (UID, cmd, sData)
-			return true, "\r\nNAME:\r\n\t!"..cmd..
-				"\r\nSYNOPSIS:\r\n\t"..cmd..
-				" [ -h ] [ scriptname ]\r\nDESCRIPTION:\r\n\t"..cmd..
-				" - command to show information for script(s). If [ scriptname ] not specified, shows information about all the scripts.\r\nOPTIONS:\r\n\t-h\t Show this help\nEXAMPLE USAGE:\r\n\t!".. cmd.." ".. Core.GetScript().sName.." - show info for one scripts.\r\n\t!"..cmd.." - show info for all scripts\r\nSEE ALSO:\r\n\t !startscript, !stopscript, !movedownscript, !moveupscript, !restartscript"
+			return true, lang[18]:gsub("%[(%S+)%]", {CMD = cmd, CURNAME=Core.GetScript().sName})
 		end,
 	},
 	
 	["movedownscript"] = {
+		options={
+			short_description=lang[19],
+		},
+	
 		["acl"] = function(UID, cmd, sData)	
 			return check_acl(UID, cmd)
 		end,
@@ -150,33 +164,34 @@ def_commands = { -- commands
 		["cmd"] = function(UID, cmd, sData)
 			if cmd then
 				if not sData or sData == "" then
-					return true, "Invalid arguments. See !"..cmd.." -h for more detals."
+					return true, lang[2]:format(cmd)
 				end
 				local ascript = Core.GetScript(sData) or {}
 				if not ascript.sName then
-					return true, "Script \""..sData.."\" not found!"
+					return true, lang[3]:format(sData)
 				else 
 					local err = false
 					local _, err = Core.MoveDownScript(ascript.sName)
 					if err then
-						return true, "Error when moveup script \"".. ascript.sName.. " \": "..err
+						return true, lang[20]:format(ascript.sName,err)
 					else
-						return true, "Script \""..ascript.sName.."\" moved up OK"
+						return true, lang[21]:format(ascript.sName)
 					end
 				end
 			end	
 		end,
 	
 		["man"] = function (UID, cmd, sData)
-			return true, "\r\nNAME:\r\n\t!"..cmd..
-				"\r\nSYNOPSIS:\r\n\t"..cmd..
-				" [ -h ] [ scriptname ]\r\nDESCRIPTION:\r\n\t"..cmd..
-				" - command lowers priority the script in the execution trees.\r\nOPTIONS:\r\n\t-h\t Show this help\nEXAMPLE USAGE:\r\n\t!".. cmd.." ".. Core.GetScript().sName.." - lowers priority for this script.\r\nSEE ALSO:\r\n\t !startscript, !stopscript, !lsscript, !moveupscript, !restartscript"
+			return true, lang[22]:gsub("%[(%S+)%]", {CMD = cmd, CURNAME=Core.GetScript().sName})
 		end,
 	
 	},
 	
 	["moveupscript"] = {
+		options={
+			short_description=lang[25],
+		},
+		
 		["acl"] = function (UID, cmd, sData)
 			return check_acl(UID, cmd)
 		end,
@@ -184,32 +199,33 @@ def_commands = { -- commands
 		["cmd"] = function(UID, cmd, sData)
 			if cmd then
 				if not sData or sData == "" then
-					return true, "Invalid arguments. See !"..cmd.." -h for more detals."
+					return true, lang[2]:format(cmd)
 				end
 				local ascript = Core.GetScript(sData) or {}
 				if not ascript.sName then
-					return true, "Script \""..sData.."\" not found!"
+					return true, lang[3]:format(sData)
 				else
 					local err = false
 					local _, err = Core.MoveUpScript(ascript.sName)
 					if err then
-						return true, "Error when moveup script \"".. ascript.sName.. " \": "..err
+						return true, lang[23]:format(ascript.sName, err)
 					else
-						return true, "Script \""..ascript.sName.."\" increase priority OK"
+						return true, lang[24]:format(ascript.sName)
 					end
 				end
 			end
 		end,
 		
 		["man"] = function (UID, cmd, sData)
-			return true,"\r\nNAME:\r\n\t!"..cmd..
-				"\r\nSYNOPSIS:\r\n\t"..cmd..
-				" [ -h ] [ scriptname ]\r\nDESCRIPTION:\r\n\t"..cmd..
-				" - command increase priority the script in the execution trees.\r\nOPTIONS:\r\n\t-h\t Show this help\nEXAMPLE USAGE:\r\n\t!".. cmd.." ".. Core.GetScript().sName.." - lowers priority for this script.\r\nSEE ALSO:\r\n\t !startscript, !stopscript, !lsscript, !movedownscript, !restartscript"
+			return true, lang[26]:gsub("%[(%S+)%]", {CMD = cmd, CURNAME=Core.GetScript().sName})
 		end,
 	},
 	
 	["restartscript"] = {
+		options={
+			short_description=lang[27],
+		},
+		
 		["acl"] = function(UID, cmd, sData)
 			return check_acl(UID, cmd)
 		end,
@@ -219,19 +235,19 @@ def_commands = { -- commands
 				if not sData then
 					local bool = Core.RestartScripts()
 					if bool then
-						return true, "All scripts successfully reloaded & reinitialized"
+						return true, lang[28]
 					end
 				elseif sData then
 					local ascript = Core.GetScript(sData) or {}
 					if not ascript.sName then
-						return true, "Script \""..sData.."\" not found!"
+						return true, lang[3]:format(sData)
 					else
 						local err = false
 						local _, err = Core.RestartScript(ascript.sName)
 						if err then
-							return true, "Error when restart script \"".. ascript.sName.. "\": "..err 
+							return true, lang[29]:format(ascript.sName, err)
 						else
-							return true, "Script \"".. ascript.sName.. "\" successfully reloaded & reinitialized"
+							return true, lang[30]:format(ascript.sName)
 						end
 					end
 				end
@@ -239,10 +255,7 @@ def_commands = { -- commands
 		end,
 		
 		["man"] = function(UID, cmd, sData)
-			return true, "\r\nNAME:\r\n\t!"..cmd..
-				"\r\nSYNOPSIS:\r\n\t"..cmd..
-				" [ -h ] [ scriptname ]\r\nDESCRIPTION:\r\n\t"..cmd..
-				" - command for restart a script(s). If [ scriptname ] not specified, restart all the scripts.\r\nOPTIONS:\r\n\t-h\t Show this help\nEXAMPLE USAGE:\r\n\t!".. cmd.." ".. Core.GetScript().sName.." - restart this script.\r\n\t!restartscript - restart all scripts.\r\nSEE ALSO:\r\n\t !startscript, !stopscript, !lsscript, !moveupscript, !movedownscript"
+			return true, lang[31]:gsub("%[(%S+)%]", {CMD = cmd, CURNAME=Core.GetScript().sName})
 		end,
 	
 	}
@@ -250,38 +263,35 @@ def_commands = { -- commands
 	
 } -- commands end
 
+-- check allow access to use 'cmd'.
+-- @return true if allowed
+-- @return false and msg if not allowed
 function check_acl(UID, cmd)
 	if cmd then
 		local profile=UID.iProfile
-		if def_acl[profile] and def_acl[profile].tcmds[cmd] then
+		if def_config.acl[profile] and def_config.acl[profile].tcmds[cmd] then
 			return true
 		else
-			return false, "You are not allowed to use this command"
+			return false, lang[32]
 		end
 	end
 end
 
-
+-- Handler data in chat.
 function OnChat(UID, sData)
-	local prefix, cmd = sData:match( "^%b<>%s+(%p)(%S+)")
-	if prefix and cmd and def_allow_prefix[prefix] and def_commands[cmd] then
+	local prefix, cmd = sData:match("^%b<>%s+(%p)(%S+)")
+	if prefix and cmd and def_config.allow_prefix[prefix] and def_commands[cmd] then
 		sData = sData:match("^%b<>%s+%p%S+%s+(.+)")
-		local aclbool, acl_reason = def_commands[cmd]["acl"](UID, cmd)
+		local aclbool, acl_reason = def_commands[cmd]["acl"](UID, cmd) -- is allowed?
 		if aclbool then
-			if sData and sData:find("^%-[Hh]$") then
-				local res,msg = def_commands[cmd]["man"](UID, cmd, sData)
-				if res then
-					Core.SendToUser(UID, msg, Config.sHubBot)
-				end
-			else
-				local res,msg = def_commands[cmd]["cmd"](UID, cmd, sData)
-				if res then 
-					Core.SendToUser(UID, msg, Config.sHubBot)
-				end
+			--  if find -H or -h keys in sData, execute 'man' function in def_commands[commandname]. If no - execute 'cmd' in def_commands[commandname].
+			local res, msg = def_commands[cmd][(sData and sData:find("^%-[Hh]$")) and "man" or "cmd"](UID, cmd, sData)
+			if res then -- if result is not none, send data to user
+				Core.SendToUser(UID, msg, botname)
 			end
 			collectgarbage("collect")
 		else
-			Core.SendToUser(UID, acl_reason, Config.sHubBot)
+			Core.SendToUser(UID, acl_reason, botname) -- if not access, send error message to user.
 		end
 		return true
 	end
@@ -289,7 +299,7 @@ function OnChat(UID, sData)
 end
 
 function OnError(s)
-	Core.SendToProfile(0,s,Config.sHubBot)
-	print(s)
-	--return true
+	Core.SendToProfile(0,s,botname)
+	print(s) -- if hub is not a daemon, show errors in console. It only for debug.
+	--return true  -- Do not stop the script at runtime errors. IT IS DANGER!!! But we can lose control. :(
 end
